7d4c3cd68a7e4f71da67ff133a6aa0e5ffb45a0c
salo322 <salo.sulaberidze7@gmail.com>
2020-06-14 11:09:39 -0700

    add code in background.js which gets all data from aliexpress

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,42 @@
-    let catId = getUrlParameter('catid');
-    let catName = getUrlParameter('catname');
+let url = window.location.href;
+
+let getUrl = function getUrl(sParam) {
+    let sPageURL = window.location.search.substring(1)
+        .toLowerCase(),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+       
+    }
+};
+    let catId = getUrl('catid');
+    let catName = getUrl('catname');
+    let searchText = getUrl('searchtext')
     let currentPage = $('.next-current').text();
-    let nextPage = parseInt(getUrlParameter('page'));
-    let newUrl = 'https://www.aliexpress.com/glosearch/api/product?CatId=' + catId + '&SearchText=' + searchText + '&catName=' + catName + 'SortType=default&page=' + nextPage + '&isrefine=y&';
+    let nextPage = parseInt(getUrl('page'));
+    let newUrl = 'https://www.aliexpress.com/glosearch/api/product?CatId=' + catId +'&SearchText=' + searchText + '&catName=' + catName + 'SortType=default&page=' + nextPage + '&isrefine=y&';
 
 
+    $.ajax({
+        url: newUrl,
+        type: 'GET',
+        dataType: 'JSON',
+        success: function(data) {
+
+           console.log(data)
+            nextPage++;
+       
+       let items = data.items;
+       console.log(items);
+      
+
+           
+
+        },
+      
+    });
+    
