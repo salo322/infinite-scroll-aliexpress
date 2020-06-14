e4e1fd4a99116c4e015cdf6775576aaf139d54fa
salo322 <salo.sulaberidze7@gmail.com>
2020-06-14 06:40:33 -0700

    Update background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,12 +1,20 @@
+    let catId = getUrlParameter('catid');
+    let catName = getUrlParameter('catname');
+    let currentPage = $('.next-current').text();
+    let nextPage = parseInt(getUrlParameter('page'));
+
+
+
+
+
 $(function () {
     $.ajax({
-        url: "https://www.aliexpress.com/glosearch/api/product?CatId=",
+        url: "'https://www.aliexpress.com/glosearch/api/product?CatId=' + catId + '&SearchText=' + searchText + '&catName=' + catName + 'SortType=default&page=' + nextPage + '&isrefine=y&'",
         type: "post",
         dataType: "html",
         success: function (data) {
-    let value  = $(data).find('.list-items');
-          console.log(value);
+    
         },
        
     });
-});
+})
