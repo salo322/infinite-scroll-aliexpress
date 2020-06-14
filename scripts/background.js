e3acbee97aa7c5b9044916b28fcddd5c88a4c2f2
salo322 <salo.sulaberidze7@gmail.com>
2020-06-14 06:44:27 -0700

    add newUrl variable

--- scripts/background.js
+++ scripts/background.js
@@ -2,19 +2,6 @@
     let catName = getUrlParameter('catname');
     let currentPage = $('.next-current').text();
     let nextPage = parseInt(getUrlParameter('page'));
+    let newUrl = 'https://www.aliexpress.com/glosearch/api/product?CatId=' + catId + '&SearchText=' + searchText + '&catName=' + catName + 'SortType=default&page=' + nextPage + '&isrefine=y&';
 
 
-
-
-
-$(function () {
-    $.ajax({
-        url: "'https://www.aliexpress.com/glosearch/api/product?CatId=' + catId + '&SearchText=' + searchText + '&catName=' + catName + 'SortType=default&page=' + nextPage + '&isrefine=y&'",
-        type: "post",
-        dataType: "html",
-        success: function (data) {
-    
-        },
-       
-    });
-})
