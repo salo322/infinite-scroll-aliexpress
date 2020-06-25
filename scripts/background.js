9c07d363df2dab9b42e4140e4490e3ad46ee0178
salo322 <salo.sulaberidze7@gmail.com>
2020-06-25 10:49:37 -0700

    change some code in component.js, manifest.json and background.js

--- scripts/background.js
+++ scripts/background.js
@@ -1,1 +1,6 @@
-
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.greeting == "hello"){
+        chrome.tabs.create({ url: request.url});
+}
+    });
--- scripts/component.js
+++ scripts/component.js
@@ -1,86 +1,93 @@
-$(window).on('load', function() {
-    let url = window.location.href;
-    let ItemComps = function(productId, aliMemberId, productDetailUrl, imageUrl, title, storeUrl, storeName, price, bigSalePrice, logisticsDesc, algoExpId, tradeDesc) {
-    let itemCode = "";
-        itemCode = '<li class="list-item" hasctr="y"><div class="list product-card" style="" product-index="0" data-product-id="' + productId + '" category-id="" ali-member-id="' + aliMemberId + '" algo-exp-id="' + algoExpId +
-                '"><div class="product-img"><a target="_blank" data-p4p="true" href="' +
-                productDetailUrl +
-                '"><img src="' + imageUrl + '" data-p4p="true" class="item-img" alt="' + title + '" width="220" height="220"></a><div class="report-btn-wrap"><span class="report-item" title="Report fraud item"></span></div><div class="atwl-btn-wrap"><a class="add-wishlist-btn" data-p4p="true"><i data-p4p="true" class="next-icon next-icon-favourite next-medium"></i></a></div></div><div class="product-info"><div class="left-zone"><div class="item-title-wrap"><a data-p4p="true" class="item-title" href="' + productDetailUrl + '" title="' + title + '" target="_blank">' + title +
-                '</a></div><div class="item-sellpoint-wrap"><div class="feature-sell-point"></div></div><div class="item-store-wrap"><a class="store-name" href="' + storeUrl + '" title="' + storeName + '" target="_blank" data-spm-anchor-id="a2g0o.productlist.0.0">' + storeName + '</a></div></div><div class="right-zone"><div class="item-price-wrap"><div class="item-price-row"><span class="price-current">' + price + '</span></div><div class="item-price-row item-warm-up"><span class="product-tag graphic"><img src="//ae01.alicdn.com/kf/HTB1zyqEbyLrK1Rjy1zd761nnpXaP.png_220x220.png_.webp" data-p4p="true" class="item-img" width="49.14285714285714" height="16px"></span><span class="price-big-sale">' + bigSalePrice + '</span></div></div><div class="item-shipping-wrap"><span class="shipping-value">' + logisticsDesc + '</span></div><div class="item-sale-wrap"><div class="sale-info without-star"><span class="sale-value"> <a data-p4p="true" rel="nofollow" class="sale-value-link" href="' + productDetailUrl + '#thf" target="_blank">' + tradeDesc + '</a> </span></div></div></div></div></div></li>';
-        itemCode = '<li class="list-item" hasctr="y"><div class="list product-card" style="" product-index="0" data-product-id="' + productId + '" category-id="" ali-member-id="' + aliMemberId + '" algo-exp-id="' + algoExpId + '"><div class="product-img"><a target="_blank" data-p4p="true" href="' + productDetailUrl + '" data-spm-anchor-id="a2g0o.productlist.0.0"><img src="' + imageUrl + '" data-p4p="true" class="item-img" alt="' + title + '" width="220" height="220" data-spm-anchor-id="a2g0o.productlist.0.i46.27feabbf8XseAm"></a><div class="report-btn-wrap"><span class="report-item" title="Report fraud item"></span></div><div class="atwl-btn-wrap"><a class="add-wishlist-btn" data-p4p="true"><i data-p4p="true" class="next-icon next-icon-favourite next-medium"></i></a></div></div><div class="product-info"><div class="left-zone"><div class="item-title-wrap"><a data-p4p="true" class="item-title" href="' + productDetailUrl + '" title="' + title + '" target="_blank">' + title + '</a></div><div class="item-sellpoint-wrap"><div class="feature-sell-point"></div></div><div class="item-store-wrap"><a class="store-name" href="' + storeUrl + '" title="' + storeName + '" target="_blank" data-spm-anchor-id="a2g0o.productlist.0.0">' + storeName + '</a></div></div><div class="right-zone"><div class="item-price-wrap"><div class="item-price-row"><span class="price-current">' + price + '</span></div><div class="item-price-row item-warm-up"><span class="product-tag graphic"><img src="//ae01.alicdn.com/kf/HTB1zyqEbyLrK1Rjy1zd761nnpXaP.png_220x220.png_.webp" data-p4p="true" class="item-img" width="49.14285714285714" height="16px"></span><span class="price-big-sale">' + bigSalePrice + '</span></div></div><div class="item-shipping-wrap"><span class="shipping-value">' + logisticsDesc + '</span></div><div class="item-sale-wrap"><div class="sale-info without-star"><span class="sale-value"> <a data-p4p="true" rel="nofollow" class="sale-value-link" href="' + productDetailUrl + '#thf" target="_blank">64 Sold</a> </span></div></div></div></div></div></li>';
-           return itemCode;
-    }
-            let getLink = function getLink(parameter) {
-                let pageHref = window.location.search.substring(1).toLowerCase(),
-                    urlVariables = pageHref.split('&'),
-                    name,
-                    i;
-            
-                for (i = 0; i < urlVariables.length; i++) {
-                    name = urlVariables[i].split('=');
-                    if (name[0] === parameter) {
-                        return name[1] === undefined ? true : decodeURIComponent(name[1]);
-                    }
+$(window).on('load', function(e) {
+           let url = window.location.href;
+           let components = function(productId, aliMemberId, productDetailUrl, imageUrl, title, storeUrl, storeName, price, bigSalePrice, logisticsDesc, algoExpId, tradeDesc) {
+           let itemComponents = "";
+               itemComponents = '<li class="list-item" hasctr="y"><div class="list product-card" style="" product-index="0" data-product-id="' + productId + '" category-id="" ali-member-id="' + aliMemberId + '" algo-exp-id="' + algoExpId +
+                    '"><div class="product-img"><a target="_blank" data-p4p="true" href="' +
+                    productDetailUrl +
+                    '"><img src="' + imageUrl + '" data-p4p="true" class="item-img" alt="' + title + '" width="220" height="220"></a><div class="report-btn-wrap"><span class="report-item" title="Report fraud item"></span></div><div class="atwl-btn-wrap"><a class="add-wishlist-btn" data-p4p="true"><i data-p4p="true" class="next-icon next-icon-favourite next-medium"></i></a></div></div><div class="product-info"><div class="left-zone"><div class="item-title-wrap"><a data-p4p="true" class="item-title" href="' + productDetailUrl + '" title="' + title + '" target="_blank">' + title +
+                    '</a></div><div class="item-sellpoint-wrap"><div class="feature-sell-point"></div></div><div class="item-store-wrap"><a class="store-name" href="' + storeUrl + '" title="' + storeName + '" target="_blank" data-spm-anchor-id="a2g0o.productlist.0.0">' + storeName + '</a></div></div><div class="right-zone"><div class="item-price-wrap"><div class="item-price-row"><span class="price-current">' + price + '</span></div><div class="item-price-row item-warm-up"><span class="product-tag graphic"><img src="//ae01.alicdn.com/kf/HTB1zyqEbyLrK1Rjy1zd761nnpXaP.png_220x220.png_.webp" data-p4p="true" class="item-img" width="49.14285714285714" height="16px"></span><span class="price-big-sale">' + bigSalePrice + '</span></div></div><div class="item-shipping-wrap"><span class="shipping-value">' + logisticsDesc + '</span></div><div class="item-sale-wrap"><div class="sale-info without-star"><span class="sale-value"> <a data-p4p="true" rel="nofollow" class="sale-value-link" href="' + productDetailUrl + '#thf" target="_blank">' + tradeDesc + '</a> </span></div></div></div></div></div></li>';
+            return itemComponents;
+          }
+           let getlink = function getlink(sParam) {
+           let pageURL = window.location.search.substring(1)
+                .toLowerCase(),
+                variables= pageURL.split('&'),
+                name,
+                i;
+             for (i = 0; i < variables.length; i++) {
+                name = variables[i].split('=');
+
+                if (name[0] === sParam) {
+                    return name[1] === undefined ? true : decodeURIComponent(name[1]);
                 }
-            };
-        
-                let catId = getLink('catid');
-                let catName = getLink('catname');
-                let searchText = getLink('searchtext');
-                let currentPage = Number( $('.next-current').text());
-                console.log(currentPage)
-                let nextPage = Number(currentPage+1);
-                console.log(nextPage)
-               
+            }
+        };
+ 
+           if (url.includes("aliexpress.com") && (url.includes("orderList") || url.includes("order_list"))) {
+                     html = document.documentElement;
+                    var currentPage = Number($($('.ui-label')[0]).text().split("/")[0]);
+                    let nextPage = currentPage + 1;
+                    let point = 0;
+                    let overed = 0;
+           if (point == 0 && overed == 0 && nextPage != currentPage) {
+                  if (scroll > 0.50 * height) {
+                          point = 1;
+                              }  }   
+                            }
+                    
+                    let overed = 0;
+                    var searchText = getlink('searchtext');
+                    let catId = getlink('catid');
+                    let catName = getlink('catname');
+                    let nextPage = parseInt(getlink('page'));
+                    (nextPage) ? nextPage++ : nextPage = 2;
+               let point = 0;
+           $(window).scroll(function(event) {
+            let height = $(document).height();
+            let scroll = $(window).scrollTop();
+               if (point == 1) {
+                  setTimeout(function() {
+                      point = 0;
+                        }, 2000);  }
+              if (point == 0 && overed == 0) {
+              if (scroll > 0.6 * height) {
+                        point = 1;
+    let requestUrl = 'https://www.aliexpress.com/glosearch/api/product?CatId=' + catId + '&SearchText=' + searchText + '&catName=' + catName + 'SortType=default&page=' + nextPage + '&isrefine=y&';
 
-                let requestUrl = 'https://www.aliexpress.com/glosearch/api/product?CatId=' + catId + '&SearchText=' + searchText + '&catName=' + catName + 'SortType=default&page=' + nextPage + '&isrefine=y&';
-               
-               
-            
-                 
+                                          $.ajax({
+                                            url: requestUrl,
+                                            type: 'GET',
+                                            dataType: 'JSON',
+                                            success: function(data) {
+                                            console.log(data.items);
+                                            nextPage++;
+                                        let allitems = data;
+                                        if (allitems) { 
+                                        let listItems = allitems.items;
 
-                
-                            let total =  $('.jump-aera, .total-page').text().match(/\d+/)
-                            let totalnum = Number(total[0]);
-                                   
-                            
-                            let arr = new Array(totalnum).fill("").map((_, nextPage) => nextPage + 1);
-                            arr.forEach(() =>
-                            $.ajax({
-                                url: requestUrl,
-                                type: 'GET',
-                                dataType:"JSON",
-                                success: $.getJSON( requestUrl, function( data ) {
-                                     
-                                let items = data.items;
-                                nextPage++;
-                                console.log(items)
-                                                        
-                             
-                                $(items).each(function(index, item) {
-                                    $('ul.list-items').append(ItemComps(item.productId, item.store.storeId, item.productDetailUrl, item.imageUrl, item.title, item.store.storeUrl, item.store.storeName, item.price, item.bigSalePrice, item.logisticsDesc, item.traceInfo.algo_expid, item.tradeDesc));
-                                   
-                                });
-                            
-                                 }),
-                                
-            
-                                error: function(error) {
-                                    console.log("Error ", error);
-                                    chrome.tabs.create({ url: requestUrl});
+                                        for (let i = 0; i < listItems.length; i++) {
+                                          if (listItems[i].adTag || listItems.adSessionId) {
+                                                 continue;
+                                                }
+                                             let item = listItems[i];
+                                         $('ul.list-items').append(components(item.productId, item.store.storeId, item.productDetailUrl, item.imageUrl, item.title, item.store.storeUrl, item.store.storeName, item.price, item.bigSalePrice, item.logisticsDesc, item.traceInfo.algo_expid, item.tradeDesc));
+                                                    }
+
+                                             let lastPage = parseInt($('.total-page').text().match(/\d+/));
+                                                if (nextPage == lastPage - 1) {
+                                                        overed = 1;
+                                                }
+                                                } },
+                                            error: function(error) {
+                                                console.log("Error ", error);
+                                                chrome.runtime.sendMessage({greeting: "hello", url:requestUrl});
+                                            }
+                                        });
+                                    }
                                 }
-                             
-                            }) );
-                            
-
+                            });
                         });
-                            
-                        
-                               
-                        
-                
-                       
-        
-    
-    
 
+
+    
