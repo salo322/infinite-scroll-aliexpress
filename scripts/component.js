3d875cbb29007e1d61d3d97ef2a280a89722bb99
salo322 <salo.sulaberidze7@gmail.com>
2020-06-26 02:49:27 -0700

    change some code

--- scripts/component.js
+++ scripts/component.js
@@ -3,9 +3,7 @@ $(window).on('load', function(e) {
            let components = function(productId, aliMemberId, productDetailUrl, imageUrl, title, storeUrl, storeName, price, bigSalePrice, logisticsDesc, algoExpId, tradeDesc) {
            let itemComponents = "";
                itemComponents = '<li class="list-item" hasctr="y"><div class="list product-card" style="" product-index="0" data-product-id="' + productId + '" category-id="" ali-member-id="' + aliMemberId + '" algo-exp-id="' + algoExpId +
-                    '"><div class="product-img"><a target="_blank" data-p4p="true" href="' +
-                    productDetailUrl +
-                    '"><img src="' + imageUrl + '" data-p4p="true" class="item-img" alt="' + title + '" width="220" height="220"></a><div class="report-btn-wrap"><span class="report-item" title="Report fraud item"></span></div><div class="atwl-btn-wrap"><a class="add-wishlist-btn" data-p4p="true"><i data-p4p="true" class="next-icon next-icon-favourite next-medium"></i></a></div></div><div class="product-info"><div class="left-zone"><div class="item-title-wrap"><a data-p4p="true" class="item-title" href="' + productDetailUrl + '" title="' + title + '" target="_blank">' + title +
+                    '"><div class="product-img"><a target="_blank" data-p4p="true" href="' + productDetailUrl +'"><img src="' + imageUrl + '" data-p4p="true" class="item-img" alt="' + title + '" width="220" height="220"></a><div class="report-btn-wrap"><span class="report-item" title="Report fraud item"></span></div><div class="atwl-btn-wrap"><a class="add-wishlist-btn" data-p4p="true"><i data-p4p="true" class="next-icon next-icon-favourite next-medium"></i></a></div></div><div class="product-info"><div class="left-zone"><div class="item-title-wrap"><a data-p4p="true" class="item-title" href="' + productDetailUrl + '" title="' + title + '" target="_blank">' + title +
                     '</a></div><div class="item-sellpoint-wrap"><div class="feature-sell-point"></div></div><div class="item-store-wrap"><a class="store-name" href="' + storeUrl + '" title="' + storeName + '" target="_blank" data-spm-anchor-id="a2g0o.productlist.0.0">' + storeName + '</a></div></div><div class="right-zone"><div class="item-price-wrap"><div class="item-price-row"><span class="price-current">' + price + '</span></div><div class="item-price-row item-warm-up"><span class="product-tag graphic"><img src="//ae01.alicdn.com/kf/HTB1zyqEbyLrK1Rjy1zd761nnpXaP.png_220x220.png_.webp" data-p4p="true" class="item-img" width="49.14285714285714" height="16px"></span><span class="price-big-sale">' + bigSalePrice + '</span></div></div><div class="item-shipping-wrap"><span class="shipping-value">' + logisticsDesc + '</span></div><div class="item-sale-wrap"><div class="sale-info without-star"><span class="sale-value"> <a data-p4p="true" rel="nofollow" class="sale-value-link" href="' + productDetailUrl + '#thf" target="_blank">' + tradeDesc + '</a> </span></div></div></div></div></div></li>';
             return itemComponents;
           }
@@ -23,19 +21,11 @@ $(window).on('load', function(e) {
                 }
             }
         };
- 
-           if (url.includes("aliexpress.com") && (url.includes("orderList") || url.includes("order_list"))) {
-                     html = document.documentElement;
-                    var currentPage = Number($($('.ui-label')[0]).text().split("/")[0]);
-                    let nextPage = currentPage + 1;
-                    let point = 0;
-                    let overed = 0;
-           if (point == 0 && overed == 0 && nextPage != currentPage) {
-                  if (scroll > 0.50 * height) {
-                          point = 1;
-                              }  }   
+               if (url) {
+                    html = document.documentElement;
+                    var currentPage = Number($($('.ui-label')[0]).text().split("/")[0]); 
                             }
-                    
+                            
                     let overed = 0;
                     var searchText = getlink('searchtext');
                     let catId = getlink('catid');
@@ -50,7 +40,7 @@ $(window).on('load', function(e) {
                   setTimeout(function() {
                       point = 0;
                         }, 2000);  }
-              if (point == 0 && overed == 0) {
+              if (point == 0 && overed == 0 && nextPage != currentPage) {
               if (scroll > 0.6 * height) {
                         point = 1;
     let requestUrl = 'https://www.aliexpress.com/glosearch/api/product?CatId=' + catId + '&SearchText=' + searchText + '&catName=' + catName + 'SortType=default&page=' + nextPage + '&isrefine=y&';
